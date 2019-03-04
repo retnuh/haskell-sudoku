3abf6ad4916e538ca2816b8b0f545edce97af01c
Hunter Kelly <retnuh@gmail.com>
2019-03-04 09:13:13 +0000

    use table-layout for nice formatting

--- driver/Main.hs
+++ driver/Main.hs
@@ -8,23 +8,31 @@ import qualified Sudoku.Puzzles                as Puzzles
 import           Sudoku.Common                  ( PuzzleResults(..) )
 import           Sudoku.Solvers
 import           Text.Printf                    ( printf )
+import           Text.Layout.Table
+
+printResults :: [RowGroup] -> IO ()
+printResults = putStrLn . tableString
+      [def, def, def, def]
+      unicodeS
+      (titlesH ["Puzzle", "Complete?", "Correct?", "Message Count"])
 
 
-printResults :: (String, PuzzleResults) -> IO ()
-printResults (n, r) = printf "%s: %s %s %d\n"
-                             (n :: String)
-                             (show (complete r))
-                             (show (correct r))
-                             (processedMessages r)
+formatResults :: (String, PuzzleResults) -> RowGroup
+formatResults (n, r) = rowG
+      [ n :: String
+      , show (complete r)
+      , show (correct r)
+      , show (processedMessages r)
+      ]
 
 
 solvers = [LSWSolver]
 
 main :: IO ()
 main = do
-  let solns =
-        [ (name, solve (solver puzzle))
-        | solver         <- solvers
-        , (name, puzzle) <- Puzzles.mostPuzzles
-        ]
-  mapM_ printResults solns
+      let solns =
+                [ (name, solve (solver puzzle))
+                | solver         <- solvers
+                , (name, puzzle) <- Puzzles.mostPuzzles
+                ]
+      printResults $ formatResults <$> solns
